c0632a77e1b95061546ed977a898071546912e6f
Matt Zhang <set.stun@gmail.com>
2022-10-24 02:51:06 -0400

    Updated workflow to zip dependencies

---
+++ .github/zip_dependencies.sh
@@ -0,0 +1,5 @@
+poetry export -f requirements.txt --without-hashes -o requirements.txt
+poetry run pip install . -r requirements.txt -t package_tmp
+cd package_tmp
+find . -name "*.pyc" -delete
+zip -r ../item_generator.zip .
