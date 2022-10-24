fab3a4d79cb7eb0389ac552376d98e93ace83e7e
Matt Zhang <set.stun@gmail.com>
2022-10-24 03:41:17 -0400

    Workflow update

--- .github/zip_dependencies.sh
+++ .github/zip_dependencies.sh
@@ -1,7 +1,7 @@
-poetry shell
 poetry export -f requirements.txt --without-hashes -o requirements.txt
 poetry run pip install . -r requirements.txt -t package_tmp
 cd package_tmp
 find . -name "*.pyc" -delete
-zip -r ../item_generator.zip .
 cd ..
+mv item_generator/lambda_function.py .
+zip -r item_generator.zip package_tmp/ lambda_function.py
