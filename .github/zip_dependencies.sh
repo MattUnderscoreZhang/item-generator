c7bfd69b8eb8a089dba3e2230202fa96795a1741
Matt Zhang <set.stun@gmail.com>
2022-10-24 23:24:23 -0400

    Lambda zip issue resolution

--- .github/zip_dependencies.sh
+++ .github/zip_dependencies.sh
@@ -1,7 +1,7 @@
 poetry export -f requirements.txt --without-hashes -o requirements.txt
-poetry run pip install . -r requirements.txt -t package_tmp
-cd package_tmp
+poetry run pip install . -r requirements.txt -t package
+cd package
 find . -name "*.pyc" -delete
+zip -r ../item_generator.zip .
 cd ..
-mv item_generator/lambda_function.py .
-zip -r item_generator.zip package_tmp/ lambda_function.py
+zip -g item_generator.zip lambda_function.py
